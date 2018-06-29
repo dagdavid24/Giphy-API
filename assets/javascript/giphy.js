bc595bbce9f0fe2a2baabc375f35173da6855ab7
Crixus <dagdavid24@yahoo.com>
2018-06-29 03:00:42 -0400

    update styling

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,50 +1,37 @@
+console.log("hi");
+
 $("button").on("click", function () {
-    // In this case, the "this" keyword refers to the button that was clicked
-    var person = $(this).attr("data-person");
-
-    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var hero = $(this).attr("data-person");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      person + "&api_key=5jb45kTi0xVsoCeW7xopxXayRod0qBfC&limit=10";
-
-    // Performing our AJAX GET request
+        hero + "&api_key=5jb45kTi0xVsoCeW7xopxXayRod0qBfC&limit=10";
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     })
-      // After the data comes back from the API
-      .then(function (response) {
-        // Storing an array of results in the results variable
-        var results = response.data;
+        .then(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var gifDiv = $("<div class='item'>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var personImage = $("<img class='gif'>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+                    gifDiv.append(p);
+                    gifDiv.append(personImage);
+                    $("#images").prepend(gifDiv);
+                }
+            }
+        });
+});
 
-        // Looping over every result item
-        for (var i = 0; i < results.length; i++) {
-
-          // Only taking action if the photo has an appropriate rating
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            // Creating a div with the class "item"
-            var gifDiv = $("<div class='item'>");
-
-            // Storing the result item's rating
-            var rating = results[i].rating;
-
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + rating);
-
-            // Creating an image tag
-            var personImage = $("<img>");
-
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            personImage.attr("src", results[i].images.fixed_height.url);
-
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
-            gifDiv.append(p);
-            gifDiv.append(personImage);
-
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#images").prepend(gifDiv);
-          }
-        }
-      });
-  });
-  console.log("hi");
+$(".gif").on("click", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
